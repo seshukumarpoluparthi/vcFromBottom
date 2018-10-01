e028b0d5ffcfb21e5b2cf90f8cf7171373237683
apple <appleapple@apples-macbook-air.local>
2018-10-01 15:44:07 +0530

    Initial Commit

---
+++ vcFromBottom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vcFromBottom
+//
+//  Created by apple on 10/1/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
